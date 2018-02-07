1c74115413201ea5ae12b22e37876fd5a41cc7c4
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-02-06 21:46:26 -0600

    New repo
    to see if errors stop occurring about class

--- ChristopherColumbus/ChristopherColumbus/src/hw2/PirateShip.java
+++ ChristopherColumbus/ChristopherColumbus/src/hw2/PirateShip.java
@@ -6,14 +6,17 @@ import java.util.Observer;
 import java.util.Random;
 
 public class PirateShip implements Observer{
-	Point shipPosition;
+	Point shipPosition ;
 	Point pirateShipPosition;
+	OceanExplorer oceanExplorer;
 	OceanMap oceanMap;
 	Random rand = new Random();
 	Ship ship;
 	
-	public PirateShip(OceanMap oceanMap) {
+	public PirateShip(OceanMap oceanMap, OceanExplorer oceanExplorer) {
+		this.oceanExplorer = oceanExplorer;
 		this.oceanMap = oceanMap;
+		pirateShipPosition = new Point(0,0);
 		
 	}
 	
@@ -24,20 +27,53 @@ public class PirateShip implements Observer{
 		if (o instanceof Ship) {
 			shipPosition = ((Ship) o).getShipLocation();
 			movePirate();
+			oceanExplorer.loadPirateShipImage();
 		}
 	}
 	
 	public void movePirate() {
 		if(pirateShipPosition.x - shipPosition.x < 0) 
-			pirateShipPosition.x++;
+			goEast();
 		
 		else
-			pirateShipPosition.x--;
+			goWest();
 		
 		if (pirateShipPosition.y - shipPosition.y < 0)
-			pirateShipPosition.y++;
+			goSouth();
 		else
-			pirateShipPosition.y--;
+			goNorth();
 	}
 	
+	public void goEast(){
+    	if(pirateShipPosition.x<oceanMap.getDimensions()-1 && oceanMap.isOcean(pirateShipPosition.x+1, pirateShipPosition.y)){
+    		pirateShipPosition.x++;
+    		
+    		
+    	}  
+    	    	
+    }
+    
+    public void goWest(){
+    	if(pirateShipPosition.x >0 && oceanMap.isOcean(pirateShipPosition.x-1, pirateShipPosition.y)){
+    		pirateShipPosition.x--;
+    		
+    	}  	
+    	
+    }
+    
+    public void goNorth(){
+    	if(pirateShipPosition.y>0 && oceanMap.isOcean(pirateShipPosition.x, pirateShipPosition.y-1)){
+    		pirateShipPosition.y--;
+    		
+    	}  	
+    	
+    }
+    
+    public void goSouth(){
+    	if(pirateShipPosition.y<oceanMap.getDimensions()-1 && oceanMap.isOcean(pirateShipPosition.x, pirateShipPosition.y+1)){
+    		pirateShipPosition.y++;
+    		
+    	}  			
+    	
+    }
 }
